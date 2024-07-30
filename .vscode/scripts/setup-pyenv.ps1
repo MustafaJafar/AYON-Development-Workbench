58416ea1d5820cc00f6518910395624e98d45687
MustafaJafar <mustafataherzaky@outlook.com>
2024-07-30 19:58:26 +0300

    add pyenv installation script

---
+++ .vscode/scripts/setup-pyenv.ps1
@@ -0,0 +1,24 @@
+# Very basic pyenv setup
+# I chose to install it in C drive as spaces in user names caused issues when using poetry.
+# More info: https://community.ynput.io/t/poetry-installation-failed/1522
+# For advanced pyenv installation script: https://github.com/pyenv-win/pyenv-win/blob/master/docs/installation.md#powershell
+
+$installation_path = "C:\.pyenv"
+Write-Host "Cloning Pyenv to $installation_path"
+git clone https://github.com/pyenv-win/pyenv-win.git $installation_path 
+
+Write-Host "Setting necessary environment variables 'PYENV', 'PYENV_ROOT' and 'PYENV_HOME' to '$installation_path\pyenv-win\'"
+[System.Environment]::SetEnvironmentVariable('PYENV',"$installation_path\pyenv-win\","User")
+[System.Environment]::SetEnvironmentVariable('PYENV_ROOT',"$installation_path\pyenv-win\","User")
+[System.Environment]::SetEnvironmentVariable('PYENV_HOME',"$installation_path\pyenv-win\","User")
+
+Write-Host "Appending '$installation_path\pyenv-win\bin' and '$installation_path\pyenv-win\shims' to 'PATH'"
+[System.Environment]::SetEnvironmentVariable('path', "$installation_path\pyenv-win\bin;" + "$installation_path\pyenv-win\shims;" + [System.Environment]::GetEnvironmentVariable('path', "User"),"User")
+
+
+# [!NOTE]
+# It's recommended to remove any python installations on the machine.
+#
+# [!WARNING]
+# VSCode overrides pyenv execuatbles making pyenv global or local (.python-version file) useless when using VScode terminal.
+# More info, https://github.com/microsoft/vscode-python/issues/20881
