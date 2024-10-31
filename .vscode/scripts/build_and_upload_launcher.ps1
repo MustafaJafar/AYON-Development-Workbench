b1288972f0745fcc48dd3921e7fcfa4ced36c701
MustafaJafar <mustafataherzaky@outlook.com>
2024-10-31 12:49:49 +0300

    add force to upload the launcher flag

--- .vscode/scripts/build_and_upload_launcher.ps1
+++ .vscode/scripts/build_and_upload_launcher.ps1
@@ -10,5 +10,5 @@ get-content $cwd\.vscode\scripts\.env | ForEach-Object {
 
 Set-Location $cwd/ayon-launcher
 
-tools/manage.ps1 --build-make-installer
+tools/manage.ps1 --build-make-installer --force
 tools/manage.ps1 upload --server $env:AYON_SERVER_URL --api-key $env:AYON_API_KEY
