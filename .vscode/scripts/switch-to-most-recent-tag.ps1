4a75a077f00a2e710477fb370925794b313e9393
MustafaJafar <mustafataherzaky@outlook.com>
2025-02-25 21:33:54 +0200

    fix a bug when getting the latest tag

--- .vscode/scripts/switch-to-most-recent-tag.ps1
+++ .vscode/scripts/switch-to-most-recent-tag.ps1
@@ -1,6 +1,8 @@
 
 Set-Location $ARGS[0]
+
+git fetch origin --tags
+
 $tag= Invoke-Expression -Command "git describe --tags --abbrev=0"
 Write-Host "Most recent tag is: $tag"
-git fetch origin $tag
 git checkout tags/$tag
